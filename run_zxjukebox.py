5b17c91256c703a399705bedfbe3f8bce37caf77
Steve Jewkes <steve@mulus.net>
2022-05-29 16:44:02 +0100

    Some pylint tidying up

--- run_zxjukebox.py
+++ run_zxjukebox.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
-
+"""
+This is a simple program that can scan for TZX and randomly select and play them continuously.
+It will also select all ZIP files in the same folder and include those in its random selection.
+"""
 import argparse
 import sys
 import os
-import glob
 import random
 import subprocess
 import time
@@ -14,11 +16,14 @@ from pathlib import Path
 from enum import Enum, auto
 
 class Filetype(Enum):
+    """
+    Define the filetypes this application supports
+    """
     UNKNONW = auto()
     TZX = auto()
     ZIP = auto()
 
-def getTZXFromPath(pathname, include_zip=False):
+def get_tzx_from_path(pathname, include_zip=False):
     """
     Returns list of TZX files from a path. Can optionally include ZIP files.
     """
@@ -27,7 +32,7 @@ def getTZXFromPath(pathname, include_zip=False):
         extensions.add(".zip")
     return [p.resolve() for p in Path(pathname).glob("**/*") if p.suffix in extensions]
 
-def getFiletype(filename):
+def get_filetype(filename):
     """
     Returns the type of file passed into it.
     """
@@ -39,7 +44,7 @@ def getFiletype(filename):
 
     return Filetype.UNKNOWN
 
-def playTZXFile(filename, args):
+def play_tzx_file(filename, args):
     """
     Plays a TZX file using the utility 'tzxplay'
     """
@@ -54,22 +59,22 @@ def playTZXFile(filename, args):
         tzxplay.append("--stop")
 
     tzxplay.append(filename)
-    subprocess.run(tzxplay)
+    subprocess.run(tzxplay, check=True)
 
-def playZIPFile(filename, args):
+def play_zip_file(filename, args):
     """
     Plays a zipped TZX file.
     """
     # Create temporary folder for ZIP
     with tempfile.TemporaryDirectory() as tempdir:
         zipfile.ZipFile(filename).extractall(tempdir)
-        tzx_files = getTZXFromPath(tempdir)
+        tzx_files = get_tzx_from_path(tempdir)
         if not tzx_files:
             print(f"No TZX files found in {filename}!")
             return
 
         game = random.choice(tzx_files)
-        playTZXFile(game, args)
+        play_tzx_file(game, args)
 
 def main():
     parser = argparse.ArgumentParser(description='Loads TZX files and uses tzxplay to play them randomly')
@@ -82,28 +87,28 @@ def main():
     parser.add_argument('--gap', type=int, default=5, help="Time (in seconds) to wait between each file (default is 5 seconds")
 
     args = parser.parse_args()
-    
+
     # Get list of TZX games from folder
     folder = os.path.abspath(args.folder)
 
     if not os.path.exists(folder):
         raise RuntimeError(f"Invalid folder: {folder}")
 
-    tzx_files = getTZXFromPath(folder, include_zip=True)
+    tzx_files = get_tzx_from_path(folder, include_zip=True)
     if not tzx_files:
         raise RuntimeError(f"No TZX files found in: {folder}")
 
     while True:
         game = random.choice(tzx_files)
-        print("Loading file: {}".format(game))
-        print("==> {}".format(os.path.basename(game)))
+        print(f"Loading file: {game}")
+        print(f"==> {os.path.basename(game)}")
 
         if not args.noplay:
-            filetype = getFiletype(game)
+            filetype = get_filetype(game)
             if filetype == Filetype.TZX:
-                playTZXFile(game, args)
+                play_tzx_file(game, args)
             elif filetype == Filetype.ZIP:
-                playZIPFile(game, args)
+                play_zip_file(game, args)
 
         print("-" * 80)
         time.sleep(args.gap)
@@ -120,4 +125,3 @@ if __name__ == '__main__':
         raise RuntimeError(f"This app requires at least python v3.7 to work (this is v{major}.{minor}")
 
     sys.exit(main())
-    
